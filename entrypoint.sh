fa4e4e4bbf09cfd0a25b50df2e24b70d7828f03d
willclev <59671948+willclev@users.noreply.github.com>
2021-06-15 11:25:46 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate the config file from environment variables
-echo "{\"app_secret\":\"$APP_SECRET\",\"pass_reqs\":$PASS_REQS,\"serial_tty\":\"$SERIAL_TTY\",\"serial_socket\":\"$SERIAL_SOCKET\"}" > /infinitude/infinitude.json
+echo "{\"app_secret\":\"$APP_SECRET\",\"pass_reqs\":$PASS_REQS,\"serial_tty\":\"$SERIAL_TTY\",\"serial_socket\":\"$SERIAL_SOCKET\"}" > /infinitude_2/infinitude.json
 
 if [ -z ${MODE+x} ]; then
 	echo "MODE is not set.  Defaulting to Production";
@@ -10,4 +10,4 @@ else
 	echo "MODE is set to $MODE";
 fi
 
-cd /infinitude && ./infinitude daemon -m $MODE
+cd /infinitude_2 && ./infinitude_2 daemon -m $MODE
